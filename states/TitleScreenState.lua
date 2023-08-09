7e531e7d4efa4036196e7e947ffbece9a9f4c839
Lucas Calabria <56711293+LucasCalabria@users.noreply.github.com>
2023-08-09 19:10:58 -0300

    Update TitleScreenState.lua para debug da tela inicial

--- states/TitleScreenState.lua
+++ states/TitleScreenState.lua
@@ -1,5 +1,8 @@
 TitleScreenState = Class{__includes = BaseState}
 
+package.path = package.path .. ";../Debug.lua"
+require 'Debug'
+
 function TitleScreenState:update(dt)
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
         gStateMachine:change('countdown') -- começa a contagem regressiva para começar o jogo
@@ -8,9 +11,30 @@ end
 
 function TitleScreenState:render()
 
-    love.graphics.setFont(flappyFont) -- escolhida para o titulo no main 
-    love.graphics.printf('Flappy Bird da Leca!', 0, 64, VIRTUAL_WIDTH, 'center')
+    setFont(flappyFont, 'flappyFont') -- escolhida para o titulo no main 
+    printf('Flappy Bird da Leca!', 'flappyFont', 0, 64, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.setFont(mediumFont) 
-    love.graphics.printf('Aperte Enter', 0, 120, VIRTUAL_WIDTH, 'center')
+    setFont(mediumFont, 'mediumFont') 
+    printf('Aperte Enter', 'mediumFont', 0, 120, VIRTUAL_WIDTH, 'center')
+
+end
+
+function printf(text, font, x, y, limit, align)
+    if TAKING_SAMPLE then
+        table.insert(print_texts, text)
+        table.insert(print_fonts, font)
+        table.insert(print_xs, x)
+        table.insert(print_ys, y)
+        table.insert(print_aligns, align)
+    end
+
+    love.graphics.setFont(Debug:getVariableObj(font))
+
+    return love.graphics.printf(text, x, y, limit, align)
+end
+
+function setFont(variable, name)
+    love.graphics.setFont(variable)
+
+    Debug:setVariableObj(name, variable)
 end
